8524680c3f2629948d42a1d872182b111a2a2c38
pedrophfsdev <fernandesphfs@gmail.com>
2021-09-02 22:22:54 -0300

    Projeto Catalogo Jogos API Finalizado

---
+++ CatalogoJogosDIO/InputModel/JogosInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.InputModel
+{
+    public class JogosInputModel
+    {
+        [Required]
+        [StringLength(150, MinimumLength = 4, ErrorMessage = "O nome do jogo deve conter entre 4 e 150 caracteres")]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 4, ErrorMessage = "O nome da produtora deve conter entre 4 e 150 caracteres")]
+        public string Produtora { get; set; }
+        [Required]
+        [Range(5, 100, ErrorMessage = "O preço deve ser de no mínimo 5 reais e no máximo 100 reais")]
+        public double Preco { get; set; }
+    }
+}
---
+++ CatalogoJogosDIO/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogosViewModel>> Obter(int pagina, int quantidade);
+            Task<JogosViewModel> Obter(Guid id);
+        Task<JogosViewModel> Inserir(JogosInputModel jogo);
+            Task Atualizar(Guid id, JogosInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
